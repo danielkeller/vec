41eadce9e5d625517e73602ca4afe1478d238dbe
Daniel Keller <dbk40@case.edu>
2012-06-23 19:10:11 -0400

    added empty phase 3

---
+++ vec/Sema3.cpp
@@ -0,0 +1,14 @@
+#include "Sema.h"
+#include "SemaNodes.h"
+#include "Error.h"
+
+#include <cassert>
+
+using namespace ast;
+using namespace sa;
+
+//Phase 3 is type inference, overload resolution, and template instantiation
+void Sema::Phase3()
+{
+
+}
