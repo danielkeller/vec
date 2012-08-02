93c578cca6465da22a95126b51ac43b60ae9331f
Daniel Keller <dbk40@case.edu>
2012-08-02 19:28:34 -0400

    switched dot output to jpg

--- make-dot.bat
+++ make-dot.bat
@@ -1,4 +1,4 @@
-dot -Tpng test2.vc.1.dot -o test2.1.png
-dot -Tpng test2.vc.2.dot -o test2.2.png
-dot -Tpng test2.vc.3.dot -o test2.3.png
-dot -Tpng test2.vc.4.dot -o test2.4.png
+dot -Tjpg test2.vc.1.dot -o test2.1.jpg
+dot -Tjpg test2.vc.2.dot -o test2.2.jpg
+dot -Tjpg test2.vc.3.dot -o test2.3.jpg
+dot -Tjpg test2.vc.4.dot -o test2.4.jpg
