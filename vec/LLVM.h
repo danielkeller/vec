fd86814ea705f36b4d812f73c7d502552769e9ce
Daniel Keller <dbk40@case.edu>
2012-11-18 16:37:40 +0000

    fixed msvc llvm inclusion

---
+++ vec/LLVM.h
@@ -0,0 +1,19 @@
+//disable all warnings for llvm on windows
+#ifdef _WIN32
+#pragma warning( push, 0 )
+#endif
+
+#include <llvm/Module.h>
+#include <llvm/Function.h>
+#include <llvm/Type.h>
+#include <llvm/DerivedTypes.h>
+#include <llvm/LLVMContext.h>
+#include <llvm/PassManager.h>
+#include <llvm/Instructions.h>
+#include <llvm/CallingConv.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+#include <llvm/Analysis/Verifier.h>
+
+#ifdef _WIN32
+#pragma warning( pop )
+#endif
--- vec/Value.cpp
+++ vec/Value.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cstring>
 
+#include "LLVM.h"
+
 namespace val{
 
 struct ValNode
